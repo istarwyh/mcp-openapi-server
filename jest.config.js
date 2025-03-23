d06c547b28cb2e02322dbc0a970034d709c9b4fc
istarwyh <yihui.wang@pku.edu.cn>
2025-03-23 18:14:29 +0800

    pass: smithery test

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ export default {
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@smithery/sdk/(.*)$': '<rootDir>/node_modules/@smithery/sdk/dist/$1',
   },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
